65bff4c61c9aecd8b799516688d242968140eacf
Vignesh <db@takeleap.in>
2022-11-07 22:34:35 +0530

    added project files

---
+++ README.md
@@ -0,0 +1,25 @@
+# Node Server Sent Events (SSE) Example
+
+This Projects illustrates the usage and guidance to implete the Server side events (SSE).
+To know about What is SSE Click here to read about it on my blog.
+
+## Installation & Starting
+
+Node Generator requires [Node.js](https://nodejs.org/) v10+ to run.
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+git clone https://github.com/dillibk777/node-server-sent-events-example.git
+cd sse-server && npm install && node app.js
+```
+Now the Backend is running on http://localhost:3000
+Open another terminal tab in the project directory and execute the below commands
+
+```sh
+cd sse-client && npm install && npm start
+```
+Now the Frontend is running on http://localhost:3001
+
+## Buy me a Coffee
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/dillibabukadati)
---
+++ sse-server/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/status', (request, response) => response.json({clients: clients.length}));
+
+const PORT = 3000;
+
+let clients = [];
+let facts = [];
+
+app.listen(PORT, () => {
+  console.log(`Facts Events service listening at http://localhost:${PORT}`)
+})
+
+// ...
+
+function eventsHandler(request, response, next) {
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  };
+  response.writeHead(200, headers);
+
+  const data = `data: ${JSON.stringify(facts)}\n\n`;
+
+  response.write(data);
+
+  const clientId = Date.now();
+
+  const newClient = {
+    id: clientId,
+    response
+  };
+
+  clients.push(newClient);
+
+  request.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter(client => client.id !== clientId);
+  });
+}
+
+app.get('/events', eventsHandler);
+
+// ...
+
+function sendEventsToAll(newFact) {
+  clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+}
+
+async function addFact(request, respsonse, next) {
+  const newFact = request.body;
+  facts.push(newFact);
+  respsonse.json(newFact)
+  return sendEventsToAll(newFact);
+}
+
+app.post('/fact', addFact);
