e59d766bac0d52358da186d5245f5c79a9f9d12f
Vignesh <db@takeleap.in>
2022-11-15 09:05:29 +0530

    updated gitignore

---
+++ sse-client/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [ facts, setFacts ] = useState([]);
+  const [ listening, setListening ] = useState(false);
+
+  useEffect( () => {
+    console.log('calling')
+    if (!listening) {
+      const events = new EventSource('http://0.0.0.0:3000/events');
+
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data);
+        
+        setFacts((facts) => facts.concat(parsedData));
+      };
+
+      setListening(true);
+    }
+  }, [listening, facts]);
+
+  return (
+    <table className="stats-table">
+      <thead>
+        <tr>
+          <th>Fact</th>
+          <th>Source</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          facts.map((fact, i) =>
+            <tr key={i}>
+              <td>{fact.info}</td>
+              <td>{fact.source}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
--- sse-server/app.js
+++ sse-server/app.js
@@ -28,20 +28,14 @@ function eventsHandler(request, response, next) {
     'Cache-Control': 'no-cache'
   };
   response.writeHead(200, headers);
-
   const data = `data: ${JSON.stringify(facts)}\n\n`;
-
   response.write(data);
-
   const clientId = Date.now();
-
   const newClient = {
     id: clientId,
     response
   };
-
   clients.push(newClient);
-
   request.on('close', () => {
     console.log(`${clientId} Connection closed`);
     clients = clients.filter(client => client.id !== clientId);
@@ -49,13 +43,9 @@ function eventsHandler(request, response, next) {
 }
 
 app.get('/events', eventsHandler);
-
-// ...
-
 function sendEventsToAll(newFact) {
   clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
 }
-
 async function addFact(request, respsonse, next) {
   const newFact = request.body;
   facts.push(newFact);
